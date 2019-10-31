a810ea442ff778e92652916a554878e2552c64c1
hyeoong96 <brain_power@naver.com>
2019-10-31 22:54:17 +0900

    please

--- drfproj/settings.py
+++ drfproj/settings.py
@@ -4,6 +4,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# admin 1234
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
